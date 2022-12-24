45202b682ab579facb34921b0091436eb531d06e
David Pérez <deividpa022@gmail.com>
2022-12-23 20:58:31 -0500

    Se finaliza ejercicio 4: métodos de cadenas de caracteres

---
+++ javascript/4.cadenas-de-texto/ejercicio4.js
@@ -0,0 +1,44 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una cadena de texto con tu Nombre
+
+// - Otra cadena de texto con tu Apellido
+
+// - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+
+// - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+
+// - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+
+// - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+
+// - Una variable que contenga la primera letra del Nombre
+
+// - Otra variable que contenga la última letra del Apellido
+
+// - Una cadena de texto que elimine los espacios de la variable "estudiante"
+
+// - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
+
+let nombre = "David";
+let apellido = "Perez";
+let estudiante = nombre.concat(" ", apellido);
+let estudianteMayus = nombre.toUpperCase();
+let estudianteMinus = nombre.toLowerCase();
+let lengthNombre = estudiante.length;
+let firstLetterName = nombre[0];
+let lastLetterLastName = apellido[apellido.length - 1];
+let estudianteSinEspacios = estudiante.replace(/ /g, "");
+let isInStudent = estudiante.includes(nombre);
+console.log(
+  nombre,
+  apellido,
+  estudiante,
+  estudianteMayus,
+  estudianteMinus,
+  lengthNombre,
+  firstLetterName,
+  lastLetterLastName,
+  estudianteSinEspacios,
+  isInStudent
+);
